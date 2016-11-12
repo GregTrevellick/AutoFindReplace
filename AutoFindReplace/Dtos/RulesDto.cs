8591408406e5b8ebe35d7c57ad350410687e5373
Gmaniacuk1 <gtrevellick@googlemail.com>
2016-11-12 06:48:15 +0000

    Correct a typo

--- AutoFindReplace/Dtos/RulesDto.cs
+++ AutoFindReplace/Dtos/RulesDto.cs
@@ -48,7 +48,7 @@
                                     var caseSensitive = CaseSensitive ? "case-sensitive" : "case-insensitive";
                                     return
                                             @"Whenever a solution named """ + SolutionName + @"""" +
-                                            @" is opened, any occurences of the text """ + FindWhat + @"""" +
+                                            @" is opened, any occurrences of the text """ + FindWhat + @"""" +
                                             @" (" + caseSensitive + ")" +
                                             @" within the file """ + FileName + @"""" +
                                             @" in the project """ + ProjectName + @"""" +
