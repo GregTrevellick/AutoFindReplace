21123095cfaf4935fafe61c771060bc2a845fffb
Gmaniacuk1 <gtrevellick@googlemail.com>
2017-01-22 13:38:17 +0000

    .

--- AutoFindReplace/Properties/AssemblyInfo.cs
+++ AutoFindReplace/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AutoFindReplace")]
+[assembly: AssemblyTitle(Vsix.Name)]
 [assembly: AssemblyDescription(Vsix.Description)]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany(Vsix.Author)]
-[assembly: AssemblyProduct("AutoFindReplace")]
+[assembly: AssemblyProduct(Vsix.Name)]
 [assembly: AssemblyCopyright(Vsix.Author)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture(Vsix.Language)]
