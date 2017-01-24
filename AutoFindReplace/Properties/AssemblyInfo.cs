cb068ba9f8ce7a213191839633550523611e721b
Gmaniacuk1 <gtrevellick@googlemail.com>
2017-01-24 06:53:39 +0000

    .

--- AutoFindReplace/Properties/AssemblyInfo.cs
+++ AutoFindReplace/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using AutoFindReplace;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -29,7 +28,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
+//[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(Vsix.Version + ".*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
